aa3d476b6b619cf3ce5ec71e2c56e5a2ea13e9d6
ulysse Gaspar <ulysse.gaspar@free.com>
2019-08-31 16:57:25 +0200

    amelioration algo

--- Nombres abondants/nombres_abondants.py
+++ Nombres abondants/nombres_abondants.py
@@ -1,7 +1,8 @@
+from math import *
 limiteMin=int(input())
 limiteMax=int(input())
 for nombre in range(limiteMin,limiteMax+1):
     somme=0
-    for y in range(1,nombre):
-        if nombre%y==0: somme+=y
+    for y in range(2,ceil(sqrt(nombre))):
+        if nombre%y==0: somme+=y+nombre/y
     if nombre<somme: print(nombre)
